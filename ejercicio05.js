e07c415f15077c2295f422a080706052c449e04c
Juan Carlos Avendaño Valenzuela <109890592+juank329@users.noreply.github.com>
2023-01-21 19:44:48 -0300

    ejercicio 05 js

---
+++ ejercicio05.js
@@ -0,0 +1,24 @@
+var alturaEnCentimetros = 173;
+
+var alturaEnMetros = alturaEnCentimetros / 100;
+console.log(alturaEnMetros);
+
+var pesoEnKilogramos = 95.5;
+
+var alturaEnMetrosRedondeadaArriba = Math.ceil(alturaEnMetros);
+console.log(alturaEnMetrosRedondeadaArriba);
+
+var pesoEnKilogramosRedondeadoAbajo = Math.floor(pesoEnKilogramos);
+console.log(pesoEnKilogramosRedondeadoAbajo);
+
+
+
+
+var maxValorJavascript = Number.MAX_VALUE;
+var maxValorJavascriptMasUno = Number.MAX_VALUE + 1;
+var comparacion = maxValorJavascriptMasUno === maxValorJavascript;
+console.log(comparacion);
+
+
+
+
